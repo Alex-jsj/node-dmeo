4d09a6bfb3b642e547ea2c8ba6a2524f0aff7a33
陈泽勇 <chenzeyongjsj@163.com>
2019-07-23 23:17:10 +0800

    暂存

---
+++ src/controller/blog.js
@@ -0,0 +1,21 @@
+const getList = (author, keyword) => {
+  return [
+    {
+      id: 1,
+      title: "test title",
+      author: "test author",
+      content: "test content",
+      createTime: 1563894287367
+    },
+    {
+      id: 2,
+      title: "test title",
+      author: "test author",
+      content: "test content",
+      createTime: 1563894287367
+    }
+  ];
+};
+module.exports = {
+  getList
+};
