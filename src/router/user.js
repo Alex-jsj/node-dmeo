340d7c5f24bcb2013da024ee9c1bb79b02c856dd
陈泽勇 <chenzeyongjsj@163.com>
2019-07-23 23:35:49 +0800

    run success

--- src/router/user.js
+++ src/router/user.js
@@ -1,10 +1,7 @@
 const handleUserRouter = (req, res) => {
   const method = req.method;
-  //获取path
-  const url = req.url;
-  const path = url.split("?")[0];
   // 登录
-  if (method === "POST" && path === "/api/blog/login") {
+  if (method === "POST" && req.path === "/api/blog/login") {
     return {
       msg: "登录"
     };
